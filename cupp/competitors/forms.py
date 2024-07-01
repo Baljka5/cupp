6cbbe231b46e1804967adf217e892c9711e9acf2
Baljka <baljinnyambaljka5@gmail.com>
2024-07-01 14:56:38 +0800

    change: competitors field

--- cupp/competitors/forms.py
+++ cupp/competitors/forms.py
@@ -4,15 +4,33 @@ from cupp.competitors.models import StoreCompetitors
 
 
 class StoreCompetitorsForm(f.ModelForm):
-    comp_open_dt = f.DateField(required=False, input_formats=settings.DATE_INPUT_FORMATS)
-    comp_close_dt = f.DateField(required=False, input_formats=settings.DATE_INPUT_FORMATS)
+    comp_open_dt = f.DateField(
+        required=False,
+        input_formats=settings.DATE_INPUT_FORMATS,
+        widget=f.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'yyyy/mm/dd',
+            'type': 'date'  # Ensure the input type is 'date' for proper formatting
+        })
+    )
+    comp_close_dt = f.DateField(
+        required=False,
+        input_formats=settings.DATE_INPUT_FORMATS,
+        widget=f.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'yyyy/mm/dd',
+            'type': 'date'
+        })
+    )
     COMP_SCHEDULE_TP_CHOICES = [
+        ('', '---------'),
         ('24H', '24H'),
         ('17H', '17H'),
         ('SPECIFIC', 'SPECIFIC')
     ]
     comp_schedule_tp = f.ChoiceField(choices=COMP_SCHEDULE_TP_CHOICES, widget=f.Select(attrs={'class': 'form-control'}),
                                      required=False)
+
     # comp_schedule_time = f.TimeField(required=False, input_formats=['%H:%M:%S'],
     #                                  widget=f.TimeInput(attrs={'type': 'time', 'step': '1'}))
 
--- cupp/competitors/models.py
+++ cupp/competitors/models.py
@@ -7,15 +7,16 @@ from django.db import models as m
 
 class StoreCompetitors(m.Model):
     store_id = m.CharField('Дэлгүүрийн дугаар', blank=True, null=True, max_length=5)
+    store_name = m.CharField('Салбарын нэр', null=True, max_length=200)
     comp_yn = m.BooleanField('Урсгалд нөлөөлөх өрсөлдөгч байгаа эсэх', blank=True, null=True)
     comp_type = m.ForeignKey('DimCompType', on_delete=m.CASCADE, blank=False, null=False)
     comp_name = m.CharField('Өрсөлдөгчийн нэр', blank=True, null=True, max_length=50)
     comp_range_meter = m.FloatField('Өрсөлдөгчийн манай дэлгүүрээс ойролцоогоор хэдэн метерт', blank=True, null=True,
                                     default=0)
     comp_cluster = m.ForeignKey('DimCluster', on_delete=m.CASCADE, blank=False, null=False)
-    comp_open_dt = m.DateField('Өрсөлдөгч хэзээ нээсэн', blank=True, null=True)
+    comp_open_dt = m.DateField('Өрсөлдөгч хэзээ нээсэн', blank=True, null=True, default='')
     comp_status = m.BooleanField('Өрсөлдөгч хэвийн ажиллаж байгаа', blank=True, null=True)
-    comp_close_dt = m.DateField('Өрсөлдөгч хаасан бол хэзээ хаасан', blank=True, null=True)
+    comp_close_dt = m.DateField('Өрсөлдөгч хаасан бол хэзээ хаасан', blank=True, null=True, default='')
     comp_schedule_tp = m.CharField('Өрсөлдөгчийн цагийн хуваарийн төрөл', blank=True, null=True, max_length=10)
     comp_schedule_time = m.CharField("Өрсөлдөгчийн цагийн хуваарь", null=True, blank=True, max_length=11)
     comp_size = m.FloatField("Өрсөлдөгчийн талбай", blank=True, null=True, default=0)
@@ -48,7 +49,7 @@ class DimCluster(m.Model):
 
 
 class DimCompType(m.Model):
-    comp_tp = m.CharField("Competitor Type",max_length=50, primary_key=True)
+    comp_tp = m.CharField("Competitor Type", max_length=50, primary_key=True)
     description = m.CharField("Description", blank=True, null=True, max_length=255)
 
     def __str__(self):
